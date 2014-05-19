e1310fbdf2b021509256589b0b22da76e1821f17
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-05-19 17:38:06 -0600

    Updated to use Accept

--- app/models/museum/case_detail.rb
+++ app/models/museum/case_detail.rb
@@ -23,7 +23,7 @@ module Museum
     end
 
     def self.follow(key)
-      detail(key).fetch('Media-Type' => 'application/vnd.github.html')
+      detail(key).fetch('Accept' => 'application/vnd.github.html')
     end
   end
 end
