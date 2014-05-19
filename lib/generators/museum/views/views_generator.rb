279a68bf1027c3b83d6e207cebe6eacd2c313e0e
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-05-19 15:38:45 -0600

    Fixed views generator.

--- lib/generators/museum/views/views_generator.rb
+++ lib/generators/museum/views/views_generator.rb
@@ -1,16 +1,18 @@
 module Museum
-  class ViewsGenerator < Rails::Generators::Base
-    source_root File.expand_path('../../../../../app/views/cases', __FILE__)
-    require File.expand_path('../../utils', __FILE__)
-    include Generators::Utils
-    
-    # all public methods in here will be run in order
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../../app/views/museum/cases', __FILE__)
+      require File.expand_path('../../utils', __FILE__)
+      include Generators::Utils
 
-    def add_views
-      output "I'm copying the default views into app/views/cases for you.", :magenta
-      template "index.html.haml", "app/views/cases/index.html.haml"
-      template "show.html.haml", "app/views/cases/show.html.haml"
-      template "_widget.html.haml", "app/views/cases/_widget.html.haml"
+      # all public methods in here will be run in order
+
+      def add_views
+        output "I'm copying the default views into app/views/museum for you.", :magenta
+        template "index.html.haml", "app/views/museum/cases/index.html.haml"
+        template "show.html.haml", "app/views/museum/cases/show.html.haml"
+        template "_widget.html.haml", "app/views/museum/cases/_widget.html.haml"
+      end
     end
   end
 end
