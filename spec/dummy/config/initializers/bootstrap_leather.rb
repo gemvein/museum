c7ca88703fa553083e0e207eb03e78fc468b2d8c
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-10-06 17:34:27 -0600

    Version bump to 0.2.3

--- spec/dummy/config/initializers/bootstrap_leather.rb
+++
@@ -1,6 +0,0 @@
-BootstrapLeather.configure do |config|
-  config.application_path = '/'
-  config.application_title = 'Dummy App'
-  config.application_description = 'Dummy description'
-  config.application_keywords = 'fake, pretend, dummy'
-end
